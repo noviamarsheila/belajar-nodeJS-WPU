f818afdb93b76b71530c36886c418ed50a65b3c6
noviamarsheila <noviasheila123@gmail.com>
2023-03-25 21:11:39 +0700

    menambahkan folder middleware

---
+++ 16-express-middleware/app.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const port = 3000;
+const expressLayouts = require("express-ejs-layouts");
+
+// gunakan ejs
+app.set("view engine", "ejs");
+
+// Third party middleware
+app.use(expressLayouts);
+app.use(morgan("dev"));
+
+// Built in middleware
+app.use(express.static("public"));
+
+// Application level middleware
+app.use((req, res, next) => {
+	console.log("Time:", Date.now());
+	next();
+});
+
+// route
+app.get("/", (req, res) => {
+	// const mahasiswa = [];
+	const mahasiswa = [
+		{
+			nama: "Sheila",
+			email: "sheila@gmail.com",
+		},
+		{
+			nama: "Berlian",
+			email: "berlian@gmail.com",
+		},
+		{
+			nama: "Novi",
+			email: "novi@gmail.com",
+		},
+	];
+	res.render("index", {
+		nama: "Sheila",
+		title: "Halaman Home",
+		mahasiswa,
+		layout: "layouts/main-layout",
+	});
+});
+app.get("/about", (req, res) => {
+	res.render("about", {
+		layout: "layouts/main-layout",
+		title: "Halaman About",
+	});
+});
+app.get("/contact", (req, res) => {
+	res.render("contact", {
+		layout: "layouts/main-layout",
+		title: "Halaman Contact",
+	});
+});
+
+app.get("/product/:id/", (req, res) => {
+	res.send(`Product ID :   ${req.params.id} <br/> Category ID : ${req.query.category}`);
+});
+
+app.use((req, res) => {
+	res.status(404);
+	res.send("<h1>404</h1>");
+});
+
+app.listen(port, () => {
+	console.log(`Example app listening at http://localhost:${port}`);
+});
